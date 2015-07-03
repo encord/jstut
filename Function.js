4442d90b97d7eb294b89f49d20a3b2b857a9f632
matej <matej@vrto.sk>
2015-07-03 17:17:43 +0200

    uprava

--- Function.js
+++ Function.js
@@ -1,20 +1,23 @@
-var pole = [1,2,3,[4,5,6,[7,8,9],10,11,12],13,14,15]
+var pole = [1,2,3,[4,5,6,[7,8,9],10,11,12],13,14,15];
 
+/**
+ *Funkcia recursion má vstupný parameter multipole a má vypísat hodnoty po poradí.
+ *
+ *Funkcia recursion ma vstupný parameter multipole a má vypísať hodnoty zaradom . 
+ *@param{Array<Array<Number>>} pole pole obsahuje čísla ktore sa majú vypísať zaradom.
+ */
 function recursion(pole)
 {
-
-    for(var j = 0; j < pole.length ; j++)
+  for(var j = 0; j < pole.length ; j++)
+  {
+    if(Array.isArray(pole[j]) === true)
     {
-      if(Array.isArray(pole[j]) === true)
-      {
-	recursion(pole[j])
-      }
-      else
-      {
-	console.log(pole[j]);
-      }
-   }  
-
+      recursion(pole[j]);
+    }
+    else
+    {
+      console.log(pole[j]);
+    }
+  }  
 }
-
 recursion(pole);
